409afc6d34f6bb67976873704979628ef5de212f
trandatnh <trandatnh@yahoo.com>
2015-07-18 02:01:06 +0800

    Use AppCompatActivity

--- app/src/main/java/com/daynhauhoc/justjava/MainActivity.java
+++ app/src/main/java/com/daynhauhoc/justjava/MainActivity.java
@@ -1,14 +1,14 @@
 package com.daynhauhoc.justjava;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
